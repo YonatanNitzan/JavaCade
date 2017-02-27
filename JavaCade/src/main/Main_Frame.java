a24fadd1a7b2c941e58bc9bf3571f22a816e73a0
yogna <yogna@YONATAN_LAPTOP>
2017-02-27 09:47:04 +0200

    Added the Main Menu's frame and panel

---
+++ JavaCade/src/main/Main_Frame.java
@@ -0,0 +1,31 @@
+package main;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Main_Frame extends JFrame{
+
+	private final static int WIDTH = 600, HEIGHT = 600;
+    private Main_Panel panel;
+	
+	public static void main(String[] args) {
+		new Main_Frame();
+	}
+	
+	public Main_Frame()
+	{
+		setTitle("JavaCade Main Menu");
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(WIDTH, HEIGHT);
+		setVisible(true);
+		
+		panel = new Main_Panel(this);
+		add(panel);
+		
+		validate();
+	}
+	
+	
+	
+}
---
+++ JavaCade/src/main/Main_Panel.java
@@ -0,0 +1,12 @@
+package main;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Main_Panel extends JPanel{
+
+	public Main_Panel(Main_Frame main_Frame) {
+		
+	}
+
+}
