874230dacb01360f44168d07b09a9a7597ce64aa
ofrimarx <ofrimarx@ofri>
2017-02-28 18:42:23 +0200

    New game-choosing system in main frame

--- JavaCade/src/main/Main_Panel.java
+++ JavaCade/src/main/Main_Panel.java
@@ -1,16 +1,63 @@
 package main;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import pacman.Pacman_Frame;
 
 @SuppressWarnings("serial")
 public class Main_Panel extends JPanel{
-
-	public Main_Panel(Main_Frame main_Frame) {
-		setBackground(Color.WHITE);
-	}
 	
+	private Main_Frame frame;
+	
+	public Main_Panel(Main_Frame main_Frame) {
+		this.frame = main_Frame;
+		
+		setBackground(Color.WHITE);
+		setLayout(new GridLayout(2,1));
+		
+		JPanel head_panel = new JPanel();
+		JPanel middle_panel = new JPanel();
+		
+		JLabel l = new JLabel("Choose a game", JLabel.CENTER);
+		l.setFont(new Font("Vijaya", Font.PLAIN, 40));
+		
+		String[] game_list = {"Pacman", "what", "what", "what"};
+		JComboBox<String> game_combo = new JComboBox<String>(game_list);
+		game_combo.setSelectedIndex(-1);
+		
+		JButton play_button = new JButton("Play");
+		play_button.setFont(new Font("Vijaya", Font.PLAIN, 25));
+		play_button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String choice = "";
+				if(game_combo.getSelectedIndex() != -1)
+					choice = game_combo.getItemAt(game_combo.getSelectedIndex());
+				
+				switch(choice){
+				case "Pacman":
+					frame.setVisible(false);
+					new Pacman_Frame();
+				}
+			}
+		});
+		
+		head_panel.add(l);
+		middle_panel.add(game_combo);
+		middle_panel.add(play_button);
+		add(head_panel);
+		add(middle_panel);
+	}
 	// TODO: make a Game_Panel class containing of the necessary functions that every game will have 
 
 }
