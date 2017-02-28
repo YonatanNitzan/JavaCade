e46631c52639831621d9a3826d87fa1597541edf
Yonatan Nitzan <Yognatan33@gmail.com>
2017-02-28 20:04:09 +0200

    Finished pacman frames

--- JavaCade/src/pacman/Player.java
+++ JavaCade/src/pacman/Player.java
@@ -1,10 +1,8 @@
 package pacman;
 
 import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Arc2D;
-import java.awt.geom.Arc2D.Double;
 
 public class Player {
 
@@ -21,13 +19,31 @@ public class Player {
 	public void paint(Graphics2D g2) {
 		g2.setColor(Color.YELLOW);
 		circle = new Arc2D.Double(x, y, 30, 30, 30, 300, Arc2D.PIE);
-		g2.fill(circle); //right
+		g2.fill(circle); //right fill
+		g2.setColor(Color.BLACK);
+		circle = new Arc2D.Double(x, y, 30, 30, 30, 300, Arc2D.PIE);
+		g2.draw(circle); //right outline
+		
+		g2.setColor(Color.YELLOW);
 		circle = new Arc2D.Double(x, y + 100, 30, 30, 300, 300, Arc2D.PIE);
-		g2.fill(circle); //down
+		g2.fill(circle); //down fill
+		g2.setColor(Color.BLACK);
+		circle = new Arc2D.Double(x, y + 100, 30, 30, 300, 300, Arc2D.PIE);
+		g2.draw(circle); //down outline
+		
+		g2.setColor(Color.YELLOW);
 		circle = new Arc2D.Double(x + 100, y, 30, 30, 210, 300, Arc2D.PIE);
-		g2.fill(circle); //left
-		circle = new Arc2D.Double(x + 100, y + 100, 30, 30, 0, 0, Arc2D.PIE);
-		g2.fill(circle); //up
+		g2.fill(circle); //left fill
+		g2.setColor(Color.BLACK);
+		circle = new Arc2D.Double(x + 100, y, 30, 30, 210, 300, Arc2D.PIE);
+		g2.draw(circle); //left outline
+		
+		g2.setColor(Color.YELLOW);
+		circle = new Arc2D.Double(x + 100, y + 100, 30, 30, 120, 300, Arc2D.PIE);
+		g2.fill(circle); //up fill
+		g2.setColor(Color.BLACK);
+		circle = new Arc2D.Double(x + 100, y + 100, 30, 30, 120, 300, Arc2D.PIE);
+		g2.draw(circle); //up outline
 	}
 
 	public void update() {
