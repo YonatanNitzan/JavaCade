34b1cfc8575a8a9e85dadc076db2bbf223e5b383
yogna <yogna@YONATAN_LAPTOP>
2017-02-28 07:52:16 +0200

    Set up the Pacman frame and panel

--- JavaCade/src/pacman/Pacman_Frame.java
+++ JavaCade/src/pacman/Pacman_Frame.java
@@ -4,7 +4,20 @@ import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class Pacman_Frame extends JFrame {
+	
+	private final static int WIDTH = 600, HEIGHT = 600;
+    private Pacman_Panel panel;
+	
 	public Pacman_Frame() {
+		setTitle("Pacman");
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(WIDTH, HEIGHT);
+		setVisible(true);
 		
+		panel = new Pacman_Panel(this);
+		add(panel);
+		
+		validate();
 	}
 }
--- JavaCade/src/pacman/Pacman_Panel.java
+++ JavaCade/src/pacman/Pacman_Panel.java
@@ -1,10 +1,13 @@
 package pacman;
 
+import java.awt.Color;
+
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Pacman_Panel extends JPanel{
-	public Pacman_Panel() {
-		
+
+	public Pacman_Panel(Pacman_Frame pacman_Frame) {
+		setBackground(Color.BLACK);
 	}
 }
